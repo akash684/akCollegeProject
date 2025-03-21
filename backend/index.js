68d4e9214a994423fd46ba325df97a7caff83746
Ajay-2k3 <ajaysettu1@gmail.com>
2025-03-21 08:10:53 +0530

    Revert "server  commit"
    This reverts commit b50977ee6cbae9f3377983aeab0d2ab1ab4e0159.

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,17 @@ const dotenv = require("dotenv");
 const app = express();
 const Routes = require("./routes/route.js");
 
-// Load environment variables
-dotenv.config();
-
 // Set PORT to a valid port number
 const PORT = process.env.PORT || 5000; // Use 5000 or any other valid port
 
+dotenv.config();
+
 // Middleware
-app.use(express.json({ limit: '10mb' })); // Parse JSON requests with a 10MB limit
+app.use(express.json({ limit: '10mb' }));
 app.use(
   cors({
-    origin: ['https://akash684.github.io', 'http://localhost:3000'], // Allow requests from these origins
-    credentials: true, // Allow cookies and credentials
+    origin: ['https://akash684.github.io', 'http://localhost:3000'],
+    credentials: true,
   })
 );
 
@@ -27,15 +26,15 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("Failed to connect to MongoDB", err));
+  .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
 // Routes
 app.use('/', Routes);
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack); // Log the error stack trace
-  res.status(500).json({ message: 'Something went wrong!' }); // Send a generic error response
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
 });
 
 // Start the server
