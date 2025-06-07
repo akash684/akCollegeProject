4a468b30b76d21a7515ccefe86a2907888560d50
akash <146053684+akash684@users.noreply.github.com>
2025-06-07 15:43:06 +0530

    Update Homepage.js

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -17,7 +17,7 @@ const Homepage = () => {
                         <StyledTitle>
                             Welcome to
                             <br />
-                            School Management
+                            School ERp
                             <br />
                             System
                         </StyledTitle>
